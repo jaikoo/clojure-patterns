09efc67c2e2744853a73823825e0c3c4a94cf901
Jonathan Conway <jonathan@jaikoo.com>
2014-04-27 11:29:46 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-patterns
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojure-patterns "0.1.0-SNAPSHOT"
+  :description "My experiments with patterns and data structures in clojureß"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
